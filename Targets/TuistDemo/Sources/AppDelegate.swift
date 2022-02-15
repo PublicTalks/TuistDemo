3e9aa2260248e781747d8152f155471e00efda7b
Hai Feng Kao <haifeng@cocoaspice.in>
2022-02-15 17:25:33 +0800

    initial commit

---
+++ Targets/TuistDemo/Sources/AppDelegate.swift
@@ -0,0 +1,25 @@
+import UIKit
+import TuistDemoKit
+import TuistDemoUI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .white
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        TuistDemoKit.hello()
+        TuistDemoUI.hello()
+
+        return true
+    }
+
+}
